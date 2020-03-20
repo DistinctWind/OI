c846b65a650895449c4be07647b062b7aaa8f362
Gorun <zq_zhouqi@qq.com>
2020-03-20 23:38:28 +0800

    done TriangleArea

--- TriangleArea.cpp
+++ TriangleArea.cpp
@@ -1,3 +1,14 @@
 /*from
 LGOJ https://www.luogu.com.cn/problem/P5708
 */
+#include <cstdio>
+#include <cmath>
+using namespace std;
+int main(){
+    double a, b, c, p, s;
+    scanf("%lf%lf%lf", &a, &b, &c);
+    p = (a+b+c)/2;
+    s = sqrt(p*(p-a)*(p-b)*(p-c));
+    printf("%.1f", s);
+    return 0;
+}
