edcf78720756c53154e32c07825c4cc694c2cdd7
Gorun <zq_zhouqi@qq.com>
2020-03-20 19:27:14 +0800

    Create and Done LetterConversion.cpp

---
+++ LetterConversion.cpp
@@ -0,0 +1,11 @@
+/*from
+LGOJ https://www.luogu.com.cn/problem/P5704
+*/
+#include <cstdio>
+using namespace std;
+int main(){
+    char x;
+    scanf("%c", &x);
+    printf("%c", x+'A'-'a');
+    return 0;
+}
