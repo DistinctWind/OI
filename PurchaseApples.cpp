93d73b72cc91b0179330b6d1564587ed84167210
Gorun <zq_zhouqi@qq.com>
2020-03-20 19:22:34 +0800

    Create PurchaseApples.cpp

---
+++ PurchaseApples.cpp
@@ -0,0 +1,11 @@
+/*from
+LGOJ https://www.luogu.com.cn/problem/P5703
+*/
+#include <cstdio>
+using namespace std;
+int main(){
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("%d", a*b);
+    return 0;
+}
