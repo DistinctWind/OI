793da8b47a8f7eb97d668067fd44dd50da73f3d9
Gorun <zq_zhouqi@qq.com>
2020-03-21 12:24:57 +0800

    done fat

--- Fat.cpp
+++ Fat.cpp
@@ -1,3 +1,18 @@
 /*from
 LGOJ https://www.luogu.com.cn/problem/P5714
 */
+#include <iostream>
+using namespace std;
+double m, h, b;
+int main(){
+    cin>>m>>h;
+    b = m/(h*h);
+    if (b<18.5) {
+        cout<<"Underweight";
+    } else if (18.5<=b&&b<24) {
+        cout<<"Normal";
+    } else if (b>=24) {
+        cout<<b<<endl<<"Overweight";
+    }
+    return 0;
+}
