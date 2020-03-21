261ad13e770d33512c97c0074b45cc949207e897
Gorun <zq_zhouqi@qq.com>
2020-03-21 09:33:31 +0800

    done Numproperty

--- Numproperty.cpp
+++ Numproperty.cpp
@@ -3,6 +3,13 @@ LGOJ https://www.luogu.com.cn/problem/P5710
 */
 #include <cstdio>
 using namespace std;
+bool c1, c2;
+int x;
 int main(){
-    
+    scanf("%d", &x);
+    c1 = !(x&1) ;
+    //原写法~(x&1)，但是这样~1有问题，还是改成!(x&1)
+    c2 = 4<x && x<=12 ;
+    printf("%d %d %d %d", c1&&c2, c1||c2, (c1&&!c2)||(!c1&&c2), !c1&&!c2);
+    return 0;
 }
