c797f6e1ab9b3215a73a6d93b8c87f16dcee4144
Gorun <zq_zhouqi@qq.com>
2020-03-23 18:06:13 +0800

    done DigitalRightTriangle

--- DigitalRightTriangle.cpp
+++ DigitalRightTriangle.cpp
@@ -1,3 +1,16 @@
 /*from
 LGOJ https://www.luogu.com.cn/problem/P5721
 */
+#include <cstdio>
+using namespace std;
+int n;
+int main() {
+    scanf("%d", &n);
+    for (int i=1, cnt=1; i<=n; i++) {
+        for (int j=i; j<=n; j++, cnt++) {
+            printf("%02d", cnt);
+        }
+        putchar('\n');
+    }
+    return 0;
+}
