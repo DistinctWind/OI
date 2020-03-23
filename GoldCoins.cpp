02f900ef65f273f6c26a7e66ca0ca8f919dd7355
Gorun <zq_zhouqi@qq.com>
2020-03-23 18:38:41 +0800

    done GoldCoins

--- GoldCoins.cpp
+++ GoldCoins.cpp
@@ -1,3 +1,19 @@
 /*from
 LGOJ https://www.luogu.com.cn/problem/P2669
 */
+#include <cstdio>
+using namespace std;
+int n, ans;
+int main() {
+    scanf("%d", &n);
+    for (int i=1, cnt=0; cnt<=n; i++) {
+        for (int j=1; j<=i; j++) {
+            if (++cnt > n) {
+                break;
+            }
+            ans += i;
+        }
+    }
+    printf("%d", ans);
+    return 0;
+}
