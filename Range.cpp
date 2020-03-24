da0aa104731b30958d4fdc87fb76205ff4a481ea
Gorun <zq_zhouqi@qq.com>
2020-03-24 22:29:06 +0800

    done Range

--- Range.cpp
+++ Range.cpp
@@ -1,3 +1,19 @@
 /*from
 LGOJ https://www.luogu.com.cn/problem/P5724
 */
+#include <cstdio>
+#include <climits>
+#include <iostream>
+using namespace std;
+const int MAXN = 101;
+int n, num, mmax=-1, mmin=INT_MAX;
+int main() {
+    scanf("%d", &n);
+    for (int i=1; i<=n; i++) {
+        scanf("%d", &num);
+        mmax = max(mmax, num);
+        mmin = min(mmin, num);
+    }
+    printf("%d", mmax-mmin);
+    return 0;
+}
