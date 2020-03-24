283b55046142b14117c28074e0e0306e689c0ca3
Gorun <zq_zhouqi@qq.com>
2020-03-24 22:24:26 +0800

    done Fbi

--- Fbi.cpp
+++ Fbi.cpp
@@ -1,3 +1,16 @@
 /*from
 LGOJ https://www.luogu.com.cn/problem/P1720
 */
+#include <cstdio>
+using namespace std;
+int n;
+long long fbi[50];
+int main() {
+    scanf("%d", &n);
+    fbi[1]=1, fbi[2]=1;
+    for (int i=3; i<=n; i++) {
+        fbi[i] = fbi[i-1]+fbi[i-2];
+    }
+    printf("%.2f", double(fbi[n]));
+    return 0;
+}
